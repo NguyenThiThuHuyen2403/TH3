1c0c6b03fe4414298467d118bfb6170ef9b480ed
PC <huyend21cntt@gmail.com>
2023-10-05 12:54:34 +0700

    Add project files.

---
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnline.Models;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        // GET: SachOnline
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+        QLBanSachEntities data = new QLBanSachEntities();
+        
+        /// <summary> /// LaySachMoi /// </summary>
+        // <param name="count">int</param>
+        /// <returns>List</returns>
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            //Lay 6 quyen sach moi
+            var listSachMoi = LaySachMoi(6); 
+            return View(listSachMoi);
+        }
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd; 
+            return PartialView(listChuDe);
+        }
+        public ActionResult SachTheoChuDe(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD==id select s;
+            return View(sach);
+        }
+        public ActionResult SachTheoNhaXuatBan(int id)
+        {
+            var nhaxuatban = from s in data.SACHes where s.MaNXB == id select s;
+            return View(nhaxuatban);
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNhaXuatBan = from cd in data.NHAXUATBANs select cd; 
+            return PartialView(listNhaXuatBan);
+        }
+        public ActionResult ChiTietSach(int id)
+        {
+            var sach = from s in data.SACHes where s.MaSach == id select s;
+            return View(sach.Single());
+        }
+    }
+
+
+}
+
+ 
